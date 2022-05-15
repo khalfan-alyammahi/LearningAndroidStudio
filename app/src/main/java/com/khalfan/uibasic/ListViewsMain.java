ebe135315be61845cdce1036ec257f77a603f626
khalfan <khalfan34747@gmail.com>
2022-05-15 21:19:23 +0400

    changed the spinner content

--- app/src/main/java/com/khalfan/uibasic/ListViewsMain.java
+++ app/src/main/java/com/khalfan/uibasic/ListViewsMain.java
@@ -60,6 +60,7 @@ public class ListViewsMain extends AppCompatActivity {
 //
 //       studentsSpinner.setAdapter(studentsAdapter);
 
+
         studentsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
